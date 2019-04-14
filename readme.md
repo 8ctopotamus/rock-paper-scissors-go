7a6d5a53074d5f78258b2d4b011e139799b8a147
8ctopotamus <joshnaylor88@gmail.com>
2019-04-14 16:14:44 -0500

    Adds .gitignore and userChoice ToLower

--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 
 ### To Develop
 `go run rps.go`
+
+### To Build Executable
+`go build rps.go`
--- rps.go
+++ rps.go
@@ -16,22 +16,21 @@ func main() {
 	losses := 0
 	ties := 0
 	fmt.Println("Rock-Paper-Scissors - GO!")
-	fmt.Println("Type r, p, or s to play.")
-	fmt.Println("-------------------------")
+	fmt.Println("Type r, p, or s to play...")
+	fmt.Println("--------------------------")
 	for {
 		fmt.Print("-> ")
 		computerChoices := [3]string{"r", "p", "s"}
-		letter, _ := reader.ReadString('\n')
-		// trimmed := strings.Replace(userChoice, "\n", "", -1) // doesn't work on windows
-		userChoice := strings.Replace(letter, "\r\n", "", -1)
+		userChoice, _ := reader.ReadString('\n')
+		// trimmed := strings.Replace(userChoice, "\n", "", -1) // doesn't work on Windows
+		userChoice = strings.Replace(userChoice, "\r\n", "", -1)
+		userChoice = strings.ToLower(userChoice)
 		fmt.Println("You played: ", userChoice)
 		if userChoice == "r" || userChoice == "p" || userChoice == "s" {
 			computerChoice := computerChoices[(rand.Intn(3-0) + 0)]
 			fmt.Println("Computer played: ", computerChoice)
 			fmt.Println("-------------")
-			if (userChoice == "r" && computerChoice == "s") ||
-				(userChoice == "p" && computerChoice == "r") ||
-				(userChoice == "s" && computerChoice == "p") {
+			if (userChoice == "r" && computerChoice == "s") || (userChoice == "p" && computerChoice == "r") || (userChoice == "s" && computerChoice == "p") {
 				wins++
 				fmt.Println("You won!")
 			} else if userChoice == computerChoice {
